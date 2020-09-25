644c57101b6176e5a934168593f6de475e5fab17
Ariel K <ar.kogan@gmail.com>
2020-09-25 20:09:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init(e) {
+function init() {
   // your code here
   var index = 0
   document.body.addEventListener('keydown', onKeyDownHandler);
