6cd72a91051dae0b7a2f34bd50fb7caaccdaad77
Ariel K <ar.kogan@gmail.com>
2020-09-25 17:55:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -20,7 +20,7 @@ function init() {
     if (key === code[index]) {
       index++;
       if (index === code.length) {
-        alert('Hurray!')
+        alert('Hurray')
         index = 0;
       }
     } else {
