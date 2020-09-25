ca854138fd16996236ec93cc32983dd1ffe0f5f6
Ariel K <ar.kogan@gmail.com>
2020-09-25 17:42:55 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,18 @@ const codes = [
 
 function init() {
   // your code here
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 'b', 'a']
+  let index = 0
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert('You win!')
+        index = 0;
+      }
+    } else {
+      index = 0
+    }
+  })
 }
