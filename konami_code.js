47eb415af79e4716b33b63be86b0606cf126b741
Ariel K <ar.kogan@gmail.com>
2020-09-25 20:12:52 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,10 @@
 
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-
-function init() {	function init(e) {
-  // Write your JavaScript code inside the init() function	  var index = 0
-
+function init(e) {
+  var index = 0
   document.body.addEventListener('keydown', function(e) {
-} 	  const key = parseInt(e.detail || e.which)
+	const key = parseInt(e.detail || e.which)
   if (key === code[index]) {
     index++
     if (index === code.length) {
