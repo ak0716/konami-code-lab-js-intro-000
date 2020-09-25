1b0c1c11200952a549c4a6eec261ed4c5fe671d6
Ariel K <ar.kogan@gmail.com>
2020-09-25 20:12:00 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,21 @@
-const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init(e) {
-  // your code here
-  var index = 0
-  document.body.addEventListener('keydown', onKeyDownHandler);
-  function onKeyDownHandler(e){
-    const key = parseInt(e.detail || e.which)
-    if (key === codes[index]) {
-      index++;
-      if (index === codes.length) {
-        alert('Hurray')
-        index = 0;
-      }
-    } else {
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+
+function init() {	function init(e) {
+  // Write your JavaScript code inside the init() function	  var index = 0
+
+  document.body.addEventListener('keydown', function(e) {
+} 	  const key = parseInt(e.detail || e.which)
+  if (key === code[index]) {
+    index++
+    if (index === code.length) {
+      alert("SUCCESS!")
       index = 0
     }
+  } else {
+    index = 0
   }
+ })
 }
