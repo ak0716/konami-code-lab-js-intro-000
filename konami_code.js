899d3b3a71efe329b0e8c8afe567b9780c4a7745
Ariel K <ar.kogan@gmail.com>
2020-09-25 19:16:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,23 @@
-const codes = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 function init() {
   // your code here
+  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
   let index = 0
   document.body.addEventListener('keydown', function(e) {
     const key = e.keyCode
-    if (key === codes[index]) {
+    if (key === code[index]) {
       index++;
       if (index === codes.length) {
         alert('Hurray')
